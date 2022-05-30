22acce7a0b017a2cb8dd45c09d12309cba73f232
Hanna413 <hanna.verkhavodka@gmail.com>
2022-05-30 22:39:57 +0300

    Revert "Домашка к уроку 4"
    This reverts commit a4a3d3ac9619e96df2e0ef19777fa18fdaf02fba.

--- task4.py
+++
@@ -1,9 +0,0 @@
-old_list = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
-
-my_dict = {}
-for item in old_list:
-    if item in my_dict:
-        my_dict[item] += 1
-    else:
-        my_dict[item] = 1
-result = [el for el in old_list if my_dict[el] == 1]
--- task5.py
+++
@@ -1,9 +0,0 @@
-from functools import reduce
-
-
-def my_multiply(a, b):
-    return a * b
-
-
-result = reduce(my_multiply, [num for num in range(100, 1001)])
-print(result)
